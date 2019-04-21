92f0853aa3d6984b9cc2517e0d7cd79397c16a78
Skink-in-Tree's-Shade <skink-in-trees-shade@users.noreply.github.com>
2019-04-21 23:24:46 +0200

    Debug table

--- test/main.c
+++ test/main.c
@@ -1,14 +1,18 @@
 #include <stdint.h>
 #include "cpu.h"
 
-static void test_rom_load(void) {
+static void test_debug_addressing(void) {
 	uint8_t rom[] = {
+		0x4A,             // LSR A
 		0xA9, 0xC4,       // LDA #C4
 		0xA5, 0xC4,       // LDA $C4
 		0xB5, 0xC4,       // LDA $C4,X
+		0xB6, 0xC4,       // LDX $C4,Y
+		0xD0, 0xC4,       // BNE *-60
 		0xAD, 0xC4, 0x00, // LDA $C400
 		0xBD, 0xC4, 0x00, // LDA $C400,X
 		0xB9, 0xC4, 0x00, // LDA $C400,Y
+		0x6C, 0xC4, 0x00, // JMP ($C400)
 		0xA1, 0xC4,       // LDA ($C4,X)
 		0xB1, 0xC4,       // LDA ($C4),Y
 		0xEA              // NOP
@@ -21,6 +25,6 @@ static void test_rom_load(void) {
 }
 
 int main(void) {
-	test_rom_load();
+	test_debug_addressing();
 	return 0;
 }
