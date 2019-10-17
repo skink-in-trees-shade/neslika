500039cdbf3a97738098be95d1f758ce1beabb36
skink-in-trees-shade <skink-in-trees-shade@users.noreply.github.com>
2019-10-17 19:03:42 +0200

    ZPX tests

---
+++ test/addressing/zpx.test.c
@@ -0,0 +1,28 @@
+#include <stdint.h>
+#include <assert.h>
+#include "util.h"
+#include "addressing/zpx.h"
+#include "zpx.test.h"
+
+void test_zpx(void) {
+    cpu_t *cpu = cpu_random();
+    cpu->memory[cpu->program_counter] = 0x24;
+    uint16_t expected = 0x24 + cpu->x;
+
+    uint16_t actual = zpx(cpu);
+
+    assert(expected == actual);
+
+    cpu_destroy(cpu);
+}
+
+void test_zpx_program_counter(void) {
+    cpu_t *cpu = cpu_random();
+    uint16_t expected = cpu->program_counter + 1;
+
+    zpx(cpu);
+
+    assert(cpu->program_counter == expected);
+
+    cpu_destroy(cpu);
+}
---
+++ test/addressing/zpx.test.h
@@ -0,0 +1,7 @@
+#ifndef ZPX_TEST_H
+#define ZPX_TEST_H
+
+void test_zpx(void);
+void test_zpx_program_counter(void);
+
+#endif
