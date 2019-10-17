9384c7191c3da2aaa76182f82d1bcdb3cdd738af
skink-in-trees-shade <skink-in-trees-shade@users.noreply.github.com>
2019-10-17 18:53:10 +0200

    ABO tests

---
+++ test/addressing/abo.test.c
@@ -0,0 +1,29 @@
+#include <stdint.h>
+#include <assert.h>
+#include "util.h"
+#include "addressing/abo.h"
+#include "abo.test.h"
+
+void test_abo(void) {
+    cpu_t *cpu = cpu_random();
+    cpu->memory[cpu->program_counter] = 0x24;
+    cpu->memory[cpu->program_counter + 1] = 0xF1;
+    uint16_t expected = 0x24F1;
+
+    uint16_t actual = abo(cpu);
+
+    assert(expected == actual);
+
+    cpu_destroy(cpu);
+}
+
+void test_abo_program_counter(void) {
+    cpu_t *cpu = cpu_random();
+    uint16_t expected = cpu->program_counter + 2;
+
+    abo(cpu);
+
+    assert(cpu->program_counter == expected);
+
+    cpu_destroy(cpu);
+}
