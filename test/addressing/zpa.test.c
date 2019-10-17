6486afc7e45e34db846b1ac71507092b7d556c1a
skink-in-trees-shade <skink-in-trees-shade@users.noreply.github.com>
2019-10-17 19:03:33 +0200

    ZPA tests

---
+++ test/addressing/zpa.test.c
@@ -0,0 +1,28 @@
+#include <stdint.h>
+#include <assert.h>
+#include "util.h"
+#include "addressing/zpa.h"
+#include "zpa.test.h"
+
+void test_zpa(void) {
+    cpu_t *cpu = cpu_random();
+    cpu->memory[cpu->program_counter] = 0x24;
+    uint16_t expected = 0x24;
+
+    uint16_t actual = zpa(cpu);
+
+    assert(expected == actual);
+
+    cpu_destroy(cpu);
+}
+
+void test_zpa_program_counter(void) {
+    cpu_t *cpu = cpu_random();
+    uint16_t expected = cpu->program_counter + 1;
+
+    zpa(cpu);
+
+    assert(cpu->program_counter == expected);
+
+    cpu_destroy(cpu);
+}
