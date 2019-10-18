f7a17e178b3c854fb01b3a23c171668eb43570a3
Skink-in-Tree's-Shade <skink-in-trees-shade@users.noreply.github.com>
2019-10-18 17:20:10 +0200

    SED

---
+++ test/instruction/sed.test.c
@@ -0,0 +1,17 @@
+#include <stdbool.h>
+#include "util.h"
+#include "instruction/sed.h"
+#include "sed.test.h"
+
+void test_sed(void) {
+    cpu_t *actual = cpu_random();
+    cpu_t *expected = cpu_clone(actual);
+    expected->decimal_mode = true;
+
+    sed(actual);
+
+    cpu_compare(expected, actual);
+
+    cpu_destroy(expected);
+    cpu_destroy(actual);
+}
