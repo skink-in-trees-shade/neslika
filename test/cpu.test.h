3b4e1a7a7ca48200ec9c207ebb012f23ce49e529
Skink-in-Tree's-Shade <skink-in-trees-shade@users.noreply.github.com>
2019-10-18 18:01:50 +0200

    CPU tests

---
+++ test/cpu.test.h
@@ -0,0 +1,22 @@
+#ifndef CPU_TEST_H
+#define CPU_TEST_H
+
+#include "cpu.h"
+
+void test_cpu_new(void);
+void test_cpu_load(void);
+
+void test_cpu_fetch(void);
+void test_cpu_decode(void);
+void test_cpu_execute(void);
+
+void test_cpu_negative_yes(void);
+void test_cpu_negative_no(void);
+
+void test_cpu_zero_yes(void);
+void test_cpu_zero_no(void);
+
+void test_cpu_running_yes(void);
+void test_cpu_running_no(void);
+
+#endif
