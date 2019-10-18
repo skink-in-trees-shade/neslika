68c8f5e695c2811be4c4d0d7c60e5982de63022d
skink-in-trees-shade <skink-in-trees-shade@users.noreply.github.com>
2019-10-18 17:16:16 +0200

    CLC

---
+++ test/instruction/clc.test.c
@@ -0,0 +1,17 @@
+#include <stdbool.h>
+#include "util.h"
+#include "instruction/clc.h"
+#include "clc.test.h"
+
+void test_clc(void) {
+    cpu_t *actual = cpu_random();
+    cpu_t *expected = cpu_clone(actual);
+    expected->carry = false;
+
+    clc(actual);
+
+    cpu_compare(expected, actual);
+
+    cpu_destroy(expected);
+    cpu_destroy(actual);
+}
