08fbd3710c52f1f37cbf0c664d51139788f38206
skink-in-trees-shade <skink-in-trees-shade@users.noreply.github.com>
2019-10-19 15:50:36 +0200

    INY -> IIY

--- lib/addressing_table.c
+++ lib/addressing_table.c
@@ -2,9 +2,9 @@
 #include "addressing/abx.h"
 #include "addressing/aby.h"
 #include "addressing/iix.h"
+#include "addressing/iiy.h"
 #include "addressing/imm.h"
 #include "addressing/imp.h"
-#include "addressing/iny.h"
 #include "addressing/zpa.h"
 #include "addressing/zpx.h"
 #include "addressing/zpy.h"
@@ -13,19 +13,19 @@
 addressing_t addressing_table[0x100] = {
 /*          0x00  0x01  0x02  0x03  0x04  0x05  0x06  0x07  0x08  0x09  0x0A  0x0B  0x0C  0x0D  0x0E  0x0F */
 /* 0x00 */  imp,  iix,  NULL, NULL, NULL, zpa,  NULL, NULL, NULL, imm,  NULL, NULL, NULL, abo,  NULL, NULL,
-/* 0x10 */  NULL, iny,  NULL, NULL, NULL, zpx,  NULL, NULL, imp,  aby,  NULL, NULL, NULL, abx,  NULL, NULL,
+/* 0x10 */  NULL, iiy,  NULL, NULL, NULL, zpx,  NULL, NULL, imp,  aby,  NULL, NULL, NULL, abx,  NULL, NULL,
 /* 0x20 */  NULL, iix,  NULL, NULL, NULL, zpa,  NULL, NULL, NULL, imm,  NULL, NULL, NULL, abo,  NULL, NULL,
-/* 0x30 */  NULL, iny,  NULL, NULL, NULL, zpx,  NULL, NULL, imp,  aby,  NULL, NULL, NULL, abx,  NULL, NULL,
+/* 0x30 */  NULL, iiy,  NULL, NULL, NULL, zpx,  NULL, NULL, imp,  aby,  NULL, NULL, NULL, abx,  NULL, NULL,
 /* 0x40 */  NULL, iix,  NULL, NULL, NULL, zpa,  NULL, NULL, NULL, imm,  NULL, NULL, NULL, abo,  NULL, NULL,
-/* 0x50 */  NULL, iny,  NULL, NULL, NULL, zpx,  NULL, NULL, imp,  aby,  NULL, NULL, NULL, abx,  NULL, NULL,
+/* 0x50 */  NULL, iiy,  NULL, NULL, NULL, zpx,  NULL, NULL, imp,  aby,  NULL, NULL, NULL, abx,  NULL, NULL,
 /* 0x60 */  NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
 /* 0x70 */  NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, imp,  NULL, NULL, NULL, NULL, NULL, NULL, NULL,
 /* 0x80 */  NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
 /* 0x90 */  NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
 /* 0xA0 */  imm,  iix,  imm,  NULL, zpa,  zpa,  zpa,  NULL, NULL, imm,  NULL, NULL, abo,  abo,  abo,  NULL,
-/* 0xB0 */  NULL, iny,  NULL, NULL, zpx,  zpx,  zpy,  NULL, imp,  aby,  NULL, NULL, abx,  abx,  aby,  NULL,
+/* 0xB0 */  NULL, iiy,  NULL, NULL, zpx,  zpx,  zpy,  NULL, imp,  aby,  NULL, NULL, abx,  abx,  aby,  NULL,
 /* 0xC0 */  imm,  iix,  NULL, NULL, zpa,  zpa,  NULL, NULL, NULL, imm,  NULL, NULL, abo,  abo,  NULL, NULL,
-/* 0xD0 */  NULL, iny,  NULL, NULL, NULL, zpx,  NULL, NULL, imp,  aby,  NULL, NULL, NULL, abx,  NULL, NULL,
+/* 0xD0 */  NULL, iiy,  NULL, NULL, NULL, zpx,  NULL, NULL, imp,  aby,  NULL, NULL, NULL, abx,  NULL, NULL,
 /* 0xE0 */  imm,  NULL, NULL, NULL, zpa,  NULL, NULL, NULL, NULL, NULL, imp,  NULL, abo,  NULL, NULL, NULL,
 /* 0xF0 */  NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, imp,  NULL, NULL, NULL, NULL, NULL, NULL, NULL
 };
---
+++ test/addressing/iiy.test.h
@@ -0,0 +1,7 @@
+#ifndef IIY_TEST_H
+#define IIY_TEST_H
+
+void test_iiy(void);
+void test_iiy_program_counter(void);
+
+#endif
