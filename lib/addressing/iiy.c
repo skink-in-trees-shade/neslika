bf4fb26566ec0173979b189201d26151ae48e25a
Skink-in-Tree's-Shade <skink-in-trees-shade@users.noreply.github.com>
2019-10-19 15:50:36 +0200

    INY -> IIY

--- lib/addressing/iiy.c
+++ lib/addressing/iiy.c
@@ -1,6 +1,6 @@
-#include "iny.h"
+#include "iiy.h"
 
-uint16_t iny(cpu_t *cpu) {
+uint16_t iiy(cpu_t *cpu) {
 	uint8_t sub = cpu->memory[cpu->program_counter++];
 	uint8_t high = cpu->memory[sub];
 	uint8_t low = cpu->memory[sub + 1];
