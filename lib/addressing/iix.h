d17557749a03a51958057f701326efe72b8615ce
skink-in-trees-shade <skink-in-trees-shade@users.noreply.github.com>
2019-10-19 15:48:18 +0200

    INX -> IIX

---
+++ lib/addressing/iix.h
@@ -0,0 +1,9 @@
+#ifndef IIX_H
+#define IIX_H
+
+#include <stdint.h>
+#include "cpu.h"
+
+uint16_t iix(cpu_t *cpu);
+
+#endif
---
+++ test/addressing/iix.test.h
@@ -0,0 +1,7 @@
+#ifndef IIX_TEST_H
+#define IIX_TEST_H
+
+void test_iix(void);
+void test_iix_program_counter(void);
+
+#endif
