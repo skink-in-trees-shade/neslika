6bf0489a8c53156417abc958a6affc16c23d0156
Skink-in-Tree's-Shade <skink-in-trees-shade@users.noreply.github.com>
2019-10-19 13:44:02 +0200

    CPY

---
+++ test/instruction/cpy.test.c
@@ -0,0 +1,58 @@
+#include <stdbool.h>
+#include "util.h"
+#include "instruction/cpy.h"
+#include "cpy.test.h"
+
+void test_cpy_carry(void) {
+    cpu_t *actual = cpu_random();
+    actual->y = 0x48;
+    actual->operand = 0x1A;
+
+    cpu_t *expected = cpu_clone(actual);
+    expected->carry = true;
+    expected->zero = false;
+    expected->negative = false;
+
+    cpy(actual);
+
+    cpu_compare(expected, actual);
+
+    cpu_destroy(expected);
+    cpu_destroy(actual);
+}
+
+void test_cpy_zero(void) {
+    cpu_t *actual = cpu_random();
+    actual->y = 0x48;
+    actual->operand = 0x48;
+
+    cpu_t *expected = cpu_clone(actual);
+    expected->carry = true;
+    expected->zero = true;
+    expected->negative = false;
+
+    cpy(actual);
+
+    cpu_compare(expected, actual);
+
+    cpu_destroy(expected);
+    cpu_destroy(actual);
+}
+
+void test_cpy_negative(void) {
+    cpu_t *actual = cpu_random();
+    actual->y = 0xEA;
+    actual->operand = 0xF8;
+
+    cpu_t *expected = cpu_clone(actual);
+    expected->carry = false;
+    expected->zero = false;
+    expected->negative = true;
+
+    cpy(actual);
+
+    cpu_compare(expected, actual);
+
+    cpu_destroy(expected);
+    cpu_destroy(actual);
+}
---
+++ test/instruction/cpy.test.h
@@ -0,0 +1,8 @@
+#ifndef CPY_TEST_H
+#define CPY_TEST_H
+
+void test_cpy_carry(void);
+void test_cpy_zero(void);
+void test_cpy_negative(void);
+
+#endif
