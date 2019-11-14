b3979cc4ce20c9b54406c42a6c2eb12c6c4f3b59
skink-in-trees-shade <skink-in-trees-shade@users.noreply.github.com>
2019-11-14 23:56:41 +0100

    PHP

---
+++ lib/instruction/php.c
@@ -0,0 +1,14 @@
+#include "php.h"
+
+void php(cpu_t *cpu) {
+	uint8_t status = 0
+		| (cpu->carry << 0)
+		| (cpu->zero << 1)
+		| (cpu->interrupt_disable << 2)
+		| (cpu->decimal_mode << 3)
+		| (1 << 4)
+		| (1 << 5)
+		| (cpu->overflow << 6)
+		| (cpu->negative << 7);
+	cpu_push(cpu, status);
+}
