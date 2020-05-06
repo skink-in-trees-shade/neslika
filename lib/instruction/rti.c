9b014b8fc8cda5650569b63865e63e54f74e3846
skink-in-trees-shade <skink-in-trees-shade@users.noreply.github.com>
2020-05-06 10:51:09 +0200

    RTI

---
+++ lib/instruction/rti.c
@@ -0,0 +1,17 @@
+#include <stdbool.h>
+#include "rti.h"
+
+void rti(cpu_t *cpu) {
+	uint8_t status = cpu_pull(cpu);
+	uint8_t low = cpu_pull(cpu);
+	uint8_t high = cpu_pull(cpu);
+
+	cpu->carry = status >> 0 & 1;
+	cpu->zero = status >> 1 & 1;
+	cpu->interrupt_disable = status >> 2 & 1;
+	cpu->decimal_mode = status >> 3 & 1;
+	cpu->break_command = false;
+	cpu->overflow = status >> 6 & 1;
+	cpu->negative = status >> 7 & 1;
+	cpu->program_counter = (high << 8) + low;
+}
