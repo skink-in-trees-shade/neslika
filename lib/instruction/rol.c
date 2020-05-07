64c1234fa79ec5ffee23773f7c7ff8f1813be26f
Skink-in-Tree's-Shade <skink-in-trees-shade@users.noreply.github.com>
2020-05-07 16:52:53 +0200

    ROL

---
+++ lib/instruction/rol.c
@@ -0,0 +1,17 @@
+#include "../addressing_mode.h"
+#include "rol.h"
+
+void rol(struct cpu *cpu) {
+	uint16_t result;
+	if (cpu->addressing_mode == addressing_implicit) {
+		result = (cpu->accumulator << 1) + (cpu->carry ? 0x01 : 0x00);
+		cpu->carry = cpu->accumulator & 0x80;
+		cpu->accumulator = result;
+	} else {
+		result = (cpu->operand << 1) + (cpu->carry ? 0x01 : 0x00);
+		cpu->carry = cpu->operand & 0x80;
+		cpu->operand = result;
+	}
+	cpu_zero(cpu, result);
+	cpu_negative(cpu, result);
+}
--- test/main.c
+++ test/main.c
@@ -47,6 +47,7 @@
 #include "instruction/php.test.h"
 #include "instruction/pla.test.h"
 #include "instruction/plp.test.h"
+#include "instruction/rol.test.h"
 #include "instruction/rti.test.h"
 #include "instruction/rts.test.h"
 #include "instruction/sec.test.h"
@@ -267,6 +268,12 @@ int main(void) {
 	runner_test(test_plp_decimal_mode);
 	runner_test(test_plp_overflow);
 	runner_test(test_plp_negative);
+
+	runner_test(test_rol_memory);
+	runner_test(test_rol_accumulator);
+	runner_test(test_rol_carry);
+	runner_test(test_rol_zero);
+	runner_test(test_rol_negative);
 
 	runner_test(test_rti);
 
