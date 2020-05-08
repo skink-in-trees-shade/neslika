f163bee70a357048ac6e898e165e78ab6a5e2e62
skink-in-trees-shade <skink-in-trees-shade@users.noreply.github.com>
2020-05-08 11:54:24 +0200

    Shifts write back to memory

--- lib/instruction/rol.c
+++ lib/instruction/rol.c
@@ -10,7 +10,7 @@ void rol(struct cpu *cpu) {
 	} else {
 		result = (cpu->operand << 1) + (cpu->carry ? 0x01 : 0x00);
 		cpu->carry = cpu->operand & 0x80;
-		cpu->operand = result;
+		cpu_poke(cpu, cpu->operand_address, result);
 	}
 	cpu_zero(cpu, result);
 	cpu_negative(cpu, result);
