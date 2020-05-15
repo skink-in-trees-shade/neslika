99cd41f3346dd95dade969ee286c4128bde6b76f
Skink-in-Tree's-Shade <skink-in-trees-shade@users.noreply.github.com>
2020-05-15 17:49:23 +0200

    TXS does not change flags

--- test/instruction/txs.test.c
+++ test/instruction/txs.test.c
@@ -9,42 +9,6 @@ void test_txs(void) {
 
 	struct cpu *expected = cpu_clone(actual);
 	expected->stack_pointer = actual->x;
-	expected->zero = false;
-	expected->negative = false;
-
-	txs(actual);
-
-	cpu_compare(expected, actual);
-
-	cpu_destroy(expected);
-	cpu_destroy(actual);
-}
-
-void test_txs_zero(void) {
-	struct cpu *actual = cpu_random();
-	actual->x = 0x00;
-
-	struct cpu *expected = cpu_clone(actual);
-	expected->stack_pointer = actual->x;
-	expected->zero = true;
-	expected->negative = false;
-
-	txs(actual);
-
-	cpu_compare(expected, actual);
-
-	cpu_destroy(expected);
-	cpu_destroy(actual);
-}
-
-void test_txs_negative(void) {
-	struct cpu *actual = cpu_random();
-	actual->x = 0xF8;
-
-	struct cpu *expected = cpu_clone(actual);
-	expected->stack_pointer = actual->x;
-	expected->zero = false;
-	expected->negative = true;
 
 	txs(actual);
 
