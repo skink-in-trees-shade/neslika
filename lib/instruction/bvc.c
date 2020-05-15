d7b7d9b072c9e0552c87f0617039895e82920512
Skink-in-Tree's-Shade <skink-in-trees-shade@users.noreply.github.com>
2020-05-15 17:47:59 +0200

    Branching respects the sign of the relative address

--- lib/instruction/bvc.c
+++ lib/instruction/bvc.c
@@ -1,7 +1,8 @@
+#include <stdint.h>
 #include "bvc.h"
 
 void bvc(struct cpu *cpu) {
 	if (!cpu->overflow) {
-		cpu->program_counter += cpu->operand;
+		cpu->program_counter += (int8_t)cpu->operand;
 	}
 }
