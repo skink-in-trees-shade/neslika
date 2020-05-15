56871500200eb8d957ababed11445221a7ae289d
Skink-in-Tree's-Shade <skink-in-trees-shade@users.noreply.github.com>
2020-05-15 17:47:59 +0200

    Branching respects the sign of the relative address

--- lib/instruction/bcs.c
+++ lib/instruction/bcs.c
@@ -1,7 +1,8 @@
+#include <stdint.h>
 #include "bcs.h"
 
 void bcs(struct cpu *cpu) {
 	if (cpu->carry) {
-		cpu->program_counter += cpu->operand;
+		cpu->program_counter += (int8_t)cpu->operand;
 	}
 }
--- lib/instruction/bpl.c
+++ lib/instruction/bpl.c
@@ -1,7 +1,8 @@
+#include <stdint.h>
 #include "bpl.h"
 
 void bpl(struct cpu *cpu) {
 	if (!cpu->negative) {
-		cpu->program_counter += cpu->operand;
+		cpu->program_counter += (int8_t)cpu->operand;
 	}
 }
