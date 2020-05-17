b16678cc7ae22dc8e2294b4ec5b7c1701e483c0d
skink-in-trees-shade <skink-in-trees-shade@users.noreply.github.com>
2020-05-17 10:41:50 +0200

    Cartridge device

---
+++ src/cartridge/cartridge.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include "cartridge.h"
+
+#pragma pack(push, 1)
+struct ines_header {
+	char signature[4];
+	uint8_t prg_rom_count;
+	uint8_t chr_rom_count;
+	bool horizontal_mirroring: 1;
+	bool has_prg_ram: 1;
+	bool has_trainer: 1;
+	bool ignore_mirroring: 1;
+	uint8_t mapper_low: 4;
+	bool vs_unisystem: 1;
+	bool playchoice10: 1;
+	uint8_t version: 2;
+	uint8_t mapper_high: 4;
+	uint8_t prg_ram_count;
+	bool tv_ntsc: 1;
+	uint8_t reserved1: 7;
+	uint8_t reserved2;
+	uint8_t reserved3[5];
+};
+#pragma pack(pop)
+
+static uint8_t _cartridge_read(struct device *device, uint16_t address) {
+	struct cartridge *cartridge = (struct cartridge *)device;
+	return cartridge->prg_rom[address & 0x3FFF];
+}
+
+static void _cartridge_write(struct device *device, uint16_t address, uint8_t value) {
+	struct cartridge *cartridge = (struct cartridge *)device;
+	cartridge->prg_rom[address & 0x3FFF] = value;
+}
+
+struct cartridge *cartridge_new(void) {
+	struct cartridge *cartridge = calloc(1, sizeof(struct cartridge));
+	cartridge->prg_rom = calloc(0x4000, sizeof(uint8_t));
+	cartridge->device.address_from = 0x8000;
+	cartridge->device.address_to = 0xFFFF;
+	cartridge->device.read = &_cartridge_read;
+	cartridge->device.write = &_cartridge_write;
+	return cartridge;
+}
+
+void cartridge_load(struct cartridge *cartridge, const char *filename) {
+	FILE *file = fopen(filename, "rb");
+
+	struct ines_header header;
+	fread(&header, sizeof(header), 1, file);
+
+	fread(cartridge->prg_rom, 0x4000, 1, file);
+
+	fclose(file);
+}
+
+void cartridge_destroy(struct cartridge *cartridge) {
+	free(cartridge->prg_rom);
+	free(cartridge);
+}
