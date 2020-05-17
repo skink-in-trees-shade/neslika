c68dbbdc8c2a946565bd50e76d6ff3329fb7e145
skink-in-trees-shade <skink-in-trees-shade@users.noreply.github.com>
2020-05-17 12:16:55 +0200

    Error checking

--- src/cartridge/cartridge.c
+++ src/cartridge/cartridge.c
@@ -4,6 +4,7 @@
 #include <stdbool.h>
 #include "mapper.h"
 #include "format/ines.h"
+#include "error.h"
 #include "cartridge.h"
 
 static uint8_t _cartridge_read(struct device *device, uint16_t address) {
@@ -20,7 +21,6 @@ static void _cartridge_write(struct device *device, uint16_t address, uint8_t va
 
 struct cartridge *cartridge_new(void) {
 	struct cartridge *cartridge = calloc(1, sizeof(struct cartridge));
-	cartridge->prg_rom = calloc(0x4000, sizeof(uint8_t));
 	cartridge->device.address_from = 0x8000;
 	cartridge->device.address_to = 0xFFFF;
 	cartridge->device.read = &_cartridge_read;
@@ -29,7 +29,13 @@ struct cartridge *cartridge_new(void) {
 }
 
 void cartridge_load(struct cartridge *cartridge, const char *filename) {
-	ines_load(cartridge, filename);
+	if (!ines_load(cartridge, filename)) {
+		error("Unknown cartridge format.");
+	}
+
+	if (mappers[cartridge->mapper].map == NULL) {
+		error("Unknown mapper %02X.", cartridge->mapper);
+	}
 }
 
 void cartridge_destroy(struct cartridge *cartridge) {
