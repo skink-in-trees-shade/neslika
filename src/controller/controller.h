3e41f7674701232e11ff01f5b68896941bb038ef
Skink-in-Tree's-Shade <skink-in-trees-shade@users.noreply.github.com>
2020-06-10 15:19:35 +0200

    Basic controller

---
+++ src/controller/controller.h
@@ -0,0 +1,21 @@
+#ifndef CONTROLLER_H
+#define CONTROLLER_H
+
+#include <stdint.h>
+#include <stdbool.h>
+#include "ppu/screen.h"
+#include "device.h"
+
+struct controller {
+	struct device device;
+	struct screen *screen;
+
+	bool write_toggle;
+	uint8_t state;
+};
+
+struct controller *controller_new(void);
+void controller_tick(struct controller *controller);
+void controller_destroy(struct controller *controller);
+
+#endif
