5103c3e4b3c2517a978a610c9983d4fab3884b9e
Skink-in-Tree's-Shade <skink-in-trees-shade@users.noreply.github.com>
2020-06-19 20:20:30 +0200

    Sprite priority

--- src/ppu/event/render_pixel.c
+++ src/ppu/event/render_pixel.c
@@ -28,7 +28,7 @@ void render_pixel(struct ppu *ppu) {
 
 				if (fg_pixel > 0x00) {
 					fg_palette = (ppu->sprite_shift_attribute[i] & 0x03) + 0x04;
-					fg_priority = !(ppu->sprite_shift_attribute[i] >> 5);
+					fg_priority = (ppu->sprite_shift_attribute[i] & 0x20) == 0x00;
 					break;
 				}
 			}
