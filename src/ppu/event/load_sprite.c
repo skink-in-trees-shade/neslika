0ae17462f87dd2b08f5fb06d1d94892151eeee1c
skink-in-trees-shade <skink-in-trees-shade@users.noreply.github.com>
2020-06-19 11:50:31 +0200

    Sprite orientation

--- src/ppu/event/load_sprite.c
+++ src/ppu/event/load_sprite.c
@@ -1,6 +1,15 @@
 #include "bus.h"
 #include "load_sprite.h"
 
+static uint8_t invert(uint8_t byte) {
+	uint8_t result = 0x00;
+	for (int i = 7; i >= 0; i--) {
+		result |= (byte & 0x01) << i;
+		byte >>= 1;
+	}
+	return result;
+}
+
 void load_sprite(struct ppu *ppu) {
 	if ((ppu->mask & 0x10) == 0x10) {
 		if (ppu->secondary_sprite > 0x00 && ppu->secondary_sprite <= 0x08) {
@@ -12,9 +21,21 @@ void load_sprite(struct ppu *ppu) {
 			uint8_t sprite_tile_id = sprite[1];
 			uint8_t sprite_attribute = sprite[2];
 
-			uint16_t address = ((ppu->control & 0x08) << 9) | (sprite_tile_id << 4) | (ppu->scanline - sprite_y);
+			uint16_t address = ((ppu->control & 0x08) << 9) | (sprite_tile_id << 4);
+
+			if ((sprite_attribute & 0x80) == 0x80) {
+				address |= 7 - (ppu->scanline - sprite_y);
+			} else {
+				address |= ppu->scanline - sprite_y;
+			}
+
 			uint8_t sprite_low = bus_read(ppu->bus, address);
 			uint8_t sprite_high = bus_read(ppu->bus, address + 0x08);
+
+			if ((sprite_attribute & 0x40) == 0x40) {
+				sprite_low = invert(sprite_low);
+				sprite_high = invert(sprite_high);
+			}
 
 			ppu->sprite_shift_low[ppu->secondary_sprite] = sprite_low;
 			ppu->sprite_shift_high[ppu->secondary_sprite] = sprite_high;
