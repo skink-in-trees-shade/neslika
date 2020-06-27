be24222b7869966405a4880e8a72976e48ce9670
skink-in-trees-shade <skink-in-trees-shade@users.noreply.github.com>
2020-06-27 17:58:24 +0200

    Sprite overflow

--- src/ppu/event/clear_oam.c
+++ src/ppu/event/clear_oam.c
@@ -2,7 +2,7 @@
 #include "clear_oam.h"
 
 void clear_oam(struct ppu *ppu) {
-	if ((ppu->mask & 0x10) == 0x10) {
+	if ((ppu->mask & 0x10) == 0x10 || (ppu->mask & 0x08) == 0x08) {
 		memset(ppu->secondary_oam, 0xFF, 0x20);
 		ppu->primary_sprite = 0x00;
 		ppu->secondary_sprite = 0x00;
--- src/ppu/event/evaluate_sprite.c
+++ src/ppu/event/evaluate_sprite.c
@@ -2,8 +2,8 @@
 #include "evaluate_sprite.h"
 
 void evaluate_sprite(struct ppu *ppu) {
-	if ((ppu->mask & 0x10) == 0x10) {
-		if (ppu->secondary_sprite < 0x08 && ppu->primary_sprite < 0x40) {
+	if ((ppu->mask & 0x10) == 0x10 || (ppu->mask & 0x08) == 0x08) {
+		if (ppu->secondary_sprite < 0x09 && ppu->primary_sprite < 0x40) {
 			uint8_t *sprite = &ppu->primary_oam[ppu->primary_sprite * 4];
 			uint8_t sprite_height = 0x08;
 			uint8_t sprite_y = sprite[0];
@@ -12,8 +12,12 @@ void evaluate_sprite(struct ppu *ppu) {
 					ppu->sprite_zero_evaluated = true;
 				}
 
-				memcpy(&ppu->secondary_oam[ppu->secondary_sprite * 4], sprite, 4);
-				ppu->secondary_sprite++;
+				if (ppu->secondary_sprite < 0x08) {
+					memcpy(&ppu->secondary_oam[ppu->secondary_sprite * 4], sprite, 4);
+					ppu->secondary_sprite++;
+				} else {
+					ppu->status |= 0x20;
+				}
 			}
 			ppu->primary_sprite++;
 		}
