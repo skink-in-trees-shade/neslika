07e2f49b6e5e117831b04bae93d9ec5f3832a6b5
skink-in-trees-shade <skink-in-trees-shade@users.noreply.github.com>
2020-07-01 17:19:25 +0200

    Two controllers

--- src/controller/controller.c
+++ src/controller/controller.c
@@ -3,22 +3,34 @@
 #include "controller.h"
 
 enum keyboard {
-	keyboard_space = 32,
-	keyboard_x = 88,
-	keyboard_z = 90,
-	keyboard_tab = 258,
-	keyboard_right = 262,
-	keyboard_left = 263,
-	keyboard_down = 264,
-	keyboard_up = 265
+	keyboard_a = 65,
+	keyboard_d = 68,
+	keyboard_e = 69,
+	keyboard_f = 70,
+	keyboard_g = 71,
+	keyboard_i = 73,
+	keyboard_j = 74,
+	keyboard_k = 75,
+	keyboard_l = 76,
+	keyboard_o = 79,
+	keyboard_q = 81,
+	keyboard_s = 83,
+	keyboard_u = 85,
+	keyboard_w = 87,
+	keyboard_semicolon = 59,
+	keyboard_apostrophe = 39
 };
 
 static uint8_t _controller_read(struct device *device, uint16_t address) {
 	struct controller *controller = (struct controller *)((char *)device - offsetof(struct controller, device));
 	
 	if (address == 0x4016) {
-		uint8_t value = controller->state & 0x01;
-		controller->state >>= 1;
+		uint8_t value = controller->state_p1 & 0x01;
+		controller->state_p1 >>= 1;
+		return value;
+	} else if (address == 0x4017) {
+		uint8_t value = controller->state_p2 & 0x01;
+		controller->state_p2 >>= 1;
 		return value;
 	}
 
@@ -31,15 +43,24 @@ static void _controller_write(struct device *device, uint16_t address, uint8_t v
 	if (address == 0x4016) {
 		controller->write_toggle = value & 0x01;
 		if (controller->write_toggle) {
-			controller->state = 0
-				| (screen_key_pressed(controller->screen, keyboard_x) << 0)
-				| (screen_key_pressed(controller->screen, keyboard_z) << 1)
-				| (screen_key_pressed(controller->screen, keyboard_tab) << 2)
-				| (screen_key_pressed(controller->screen, keyboard_space) << 3)
-				| (screen_key_pressed(controller->screen, keyboard_up) << 4)
-				| (screen_key_pressed(controller->screen, keyboard_down) << 5)
-				| (screen_key_pressed(controller->screen, keyboard_left) << 6)
-				| (screen_key_pressed(controller->screen, keyboard_right) << 7);
+			controller->state_p1 = 0
+				| (screen_key_pressed(controller->screen, keyboard_f) << 0)
+				| (screen_key_pressed(controller->screen, keyboard_g) << 1)
+				| (screen_key_pressed(controller->screen, keyboard_q) << 2)
+				| (screen_key_pressed(controller->screen, keyboard_e) << 3)
+				| (screen_key_pressed(controller->screen, keyboard_w) << 4)
+				| (screen_key_pressed(controller->screen, keyboard_s) << 5)
+				| (screen_key_pressed(controller->screen, keyboard_a) << 6)
+				| (screen_key_pressed(controller->screen, keyboard_d) << 7);
+			controller->state_p2 = 0
+				| (screen_key_pressed(controller->screen, keyboard_semicolon) << 0)
+				| (screen_key_pressed(controller->screen, keyboard_apostrophe) << 1)
+				| (screen_key_pressed(controller->screen, keyboard_u) << 2)
+				| (screen_key_pressed(controller->screen, keyboard_o) << 3)
+				| (screen_key_pressed(controller->screen, keyboard_i) << 4)
+				| (screen_key_pressed(controller->screen, keyboard_k) << 5)
+				| (screen_key_pressed(controller->screen, keyboard_j) << 6)
+				| (screen_key_pressed(controller->screen, keyboard_l) << 7);
 		}
 	}
 }
@@ -47,7 +68,7 @@ static void _controller_write(struct device *device, uint16_t address, uint8_t v
 struct controller *controller_new(void) {
 	struct controller *controller = calloc(1, sizeof(struct controller));
 	controller->device.address_from = 0x4016;
-	controller->device.address_to = 0x4016;
+	controller->device.address_to = 0x4017;
 	controller->device.read = &_controller_read;
 	controller->device.write = &_controller_write;
 	return controller;
