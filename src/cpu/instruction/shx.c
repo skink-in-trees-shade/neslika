d6ef06c30a67d5c745711d8189c9a04586886d4e
skink-in-trees-shade <skink-in-trees-shade@users.noreply.github.com>
2020-07-01 14:09:47 +0200

    SHX

---
+++ src/cpu/instruction/shx.c
@@ -0,0 +1,9 @@
+#include "shx.h"
+
+void shx(struct cpu *cpu) {
+	uint8_t operand_low = cpu->operand & 0xFF;
+	uint8_t operand_high = cpu->operand >> 8;
+	uint8_t value = cpu->x & (operand_high + 1);
+	uint16_t address = (value << 8) | operand_low;
+	cpu_poke(cpu, address, value);
+}
---
+++ src/cpu/instruction/shx.h
@@ -0,0 +1,8 @@
+#ifndef SHX_H
+#define SHX_H
+
+#include "cpu/cpu.h"
+
+void shx(struct cpu *cpu);
+
+#endif
