5057be3106d8378f07dec9249e564a3ad940cd6d
skink-in-trees-shade <skink-in-trees-shade@users.noreply.github.com>
2020-09-03 11:38:07 +0200

    OAM attribute mask

--- src/ppu/memory/write_oam_data.c
+++ src/ppu/memory/write_oam_data.c
@@ -5,5 +5,8 @@ void write_oam_data(void *device, uint16_t address, uint8_t value) {
 	(void)address;
 
 	struct ppu *ppu = device;
+	if ((ppu->oam_address & 0x03) == 0x02) {
+		value &= 0xE3;
+	}
 	ppu->primary_oam[ppu->oam_address++] = value;
 }
