927202e82c51ada6e7aab2f165483f7d9909eae2
skink-in-trees-shade <skink-in-trees-shade@users.noreply.github.com>
2020-09-03 11:37:46 +0200

    Palette read quirk

--- src/ppu/memory/read_data.c
+++ src/ppu/memory/read_data.c
@@ -9,6 +9,7 @@ uint8_t read_data(void *device, uint16_t address) {
 	ppu->read_buffer = bus_read(ppu->ppu_bus, ppu->vram_address);
 	if ((ppu->vram_address & 0x3F00) == 0x3F00) {
 		value = ppu->read_buffer;
+		ppu->read_buffer = bus_read(ppu->ppu_bus, ppu->vram_address & 0x2FFF);
 	}
 	ppu->vram_address += (ppu->control & 0x04) == 0x04 ? 32 : 1;
 	return value;
