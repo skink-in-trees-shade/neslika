7a26d0d22350260ea8f7696aca8b1d738b9173a7
skink-in-trees-shade <skink-in-trees-shade@users.noreply.github.com>
2020-09-11 15:19:15 +0200

    PPU open bus fixes

--- src/ppu/memory/read_status.c
+++ src/ppu/memory/read_status.c
@@ -6,7 +6,7 @@ uint8_t read_status(void *device, uint16_t address) {
 
 	struct ppu *ppu = device;
 
-	uint8_t value = (ppu->status & 0xE0) | (ppu->read_buffer & 0x1F);
+	uint8_t value = (ppu->status & 0xE0) | (ppu->last_value & 0x1F);
 	ppu->status &= 0x7F;
 	ppu->write_toggle = false;
 	ppu->last_value = value;
