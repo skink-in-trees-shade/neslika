9bfbdf3a1090befbb84d4eeb4f5fb845580e5dfe
Vadim Zaslavsky <vadim.zaslavsky@l-p-a.com>
2021-02-17 01:37:23 +0100

    d3d9

--- src/main.c
+++ src/main.c
@@ -2,7 +2,7 @@
 
 int main(int argc, char *argv[]) {
 	struct neslika *nes = neslika_new();
-	neslika_load(nes, "../robocop-3.nes");
+	neslika_load(nes, "../chip-and-dale.nes");
 	neslika_run(nes);
 	neslika_destroy(nes);
 	return 0;
--- src/platform/windows/screen.c
+++ src/platform/windows/screen.c
@@ -1,6 +1,8 @@
 #define WIN32_LEAN_AND_MEAN
 #undef UNICODE
 #include <windows.h>
+#define D3D_DISABLE_9EX
+#include <d3d9.h>
 #include <stdlib.h>
 #include "platform/screen.h"
 
@@ -8,14 +10,17 @@ struct screen {
 	int width;
 	int height;
 	HWND hWnd;
-	HBITMAP hBitmap;
 	DWORD *pixels;
+	IDirect3D9 *context;
+	IDirect3DDevice9 *device;
+	IDirect3DSurface9 *surface;
 };
 
 struct screen *screen_new(char *title, int width, int height) {
 	struct screen *screen = calloc(1, sizeof(struct screen));
 	screen->width = width;
 	screen->height = height;
+	screen->pixels = calloc(screen->width * screen->height, sizeof(DWORD));
 
 	HINSTANCE hInstance = GetModuleHandle(NULL);
 
@@ -30,21 +35,14 @@ struct screen *screen_new(char *title, int width, int height) {
 	AdjustWindowRect(&r, dwStyle, FALSE);
 	screen->hWnd = CreateWindow(title, title, dwStyle, CW_USEDEFAULT, CW_USEDEFAULT, r.right - r.left, r.bottom - r.top, NULL, NULL, hInstance, NULL);
 
-	HDC hDc = GetDC(screen->hWnd);
-	HDC hDcMem = CreateCompatibleDC(hDc);
+	screen->context = Direct3DCreate9(D3D_SDK_VERSION);
 
-	BITMAPINFO bmi;
-	ZeroMemory(&bmi,sizeof(BITMAPINFO));
-	bmi.bmiHeader.biSize = sizeof(BITMAPINFOHEADER);
-	bmi.bmiHeader.biWidth = screen->width;
-	bmi.bmiHeader.biHeight = -screen->height;
-	bmi.bmiHeader.biPlanes = 1;
-	bmi.bmiHeader.biBitCount = 32;
-
-	screen->hBitmap = CreateDIBSection(hDc, &bmi, DIB_RGB_COLORS, (void **)&screen->pixels, NULL, 0);
-
-	DeleteDC(hDcMem);
-	ReleaseDC(screen->hWnd, hDc);
+	D3DPRESENT_PARAMETERS parameters = {0};
+	parameters.hDeviceWindow = screen->hWnd;
+	parameters.Windowed = TRUE;
+	parameters.SwapEffect = D3DSWAPEFFECT_DISCARD;
+	IDirect3D9_CreateDevice(screen->context, D3DADAPTER_DEFAULT, D3DDEVTYPE_HAL, NULL, D3DCREATE_SOFTWARE_VERTEXPROCESSING, &parameters, &screen->device);
+	IDirect3DDevice9_CreateOffscreenPlainSurface(screen->device, screen->width, screen->height, D3DFMT_X8R8G8B8, D3DPOOL_DEFAULT, &screen->surface, NULL);
 
 	return screen;
 }
@@ -66,19 +64,24 @@ void screen_update(struct screen *screen) {
 }
 
 void screen_commit(struct screen *screen) {
-	HDC hDc = GetDC(screen->hWnd);
-	HDC hDcMem = CreateCompatibleDC(hDc);
-	HBITMAP hOldBitmap = SelectObject(hDcMem, screen->hBitmap);
+	D3DLOCKED_RECT rect;
+	IDirect3DSurface9_LockRect(screen->surface, &rect, NULL, 0);
+	CopyMemory(rect.pBits, screen->pixels, screen->width * screen->height * sizeof(DWORD));
+	IDirect3DSurface9_UnlockRect(screen->surface);
 
-	StretchBlt(hDc, 0, 0, screen->width * 2, screen->height * 2, hDcMem, 0, 0, screen->width, screen->height, SRCCOPY);
+	IDirect3DSurface9 *back_buffer;
+	IDirect3DDevice9_GetBackBuffer(screen->device, 0, 0, D3DBACKBUFFER_TYPE_MONO, &back_buffer);
+	IDirect3DDevice9_StretchRect(screen->device, screen->surface, NULL, back_buffer, NULL, D3DTEXF_POINT);
+	IDirect3DSurface9_Release(back_buffer);
 
-	SelectObject(hDcMem, hOldBitmap);
-	DeleteDC(hDcMem);
-	ReleaseDC(screen->hWnd, hDc);
+	IDirect3DDevice9_Present(screen->device, NULL, NULL, NULL, NULL);
 }
 
 void screen_destroy(struct screen *screen) {
-	DeleteObject(screen->hBitmap);
+	IDirect3DSurface9_Release(screen->surface);
+	IDirect3DDevice9_Release(screen->device);
+	IDirect3D9_Release(screen->context);
 	DestroyWindow(screen->hWnd);
+	free(screen->pixels);
 	free(screen);
 }
