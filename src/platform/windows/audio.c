3cdf841ca0f90233873fae24a36a78518538c4ac
Vadim Zaslavsky <vadim.zaslavsky@l-p-a.com>
2021-02-17 09:51:48 +0100

    nosdl

--- src/platform/windows/audio.c
+++ src/platform/windows/audio.c
@@ -1,5 +1,6 @@
+#include <windows.h>
 #include <stdlib.h>
-#include <SDL.h>
+#include <mmreg.h>
 #include "platform/audio.h"
 
 const long apu_frequency = 1786840; // 1789773;
@@ -7,7 +8,7 @@ const long apu_frequency = 1786840; // 1789773;
 #define BUF_SIZE 800
 
 struct audio {
-	SDL_AudioDeviceID device;
+	HWAVEOUT hWaveOut;
 	uint16_t rate;
 	float *samples;
 	size_t samples_count;
@@ -15,21 +16,32 @@ struct audio {
 	float acc;
 };
 
+static void CALLBACK waveOutProc(HWAVEOUT hWaveOut, UINT uMsg, DWORD_PTR dwInstance, DWORD_PTR dwParam1, DWORD_PTR dwParam2) {
+	(void)dwInstance;
+	(void)dwParam2;
+
+	if (uMsg == WOM_DONE) {
+		LPWAVEHDR lpWaveHdr = (LPWAVEHDR)dwParam1;
+		waveOutUnprepareHeader(hWaveOut, lpWaveHdr, sizeof(WAVEHDR));
+		free(lpWaveHdr->lpData);
+		free(lpWaveHdr);
+	}
+}
+
 struct audio *audio_new(uint8_t channels, uint16_t rate) {
 	struct audio *audio = calloc(1, sizeof(struct audio));
 	audio->rate = rate;
 	audio->dt = (float)apu_frequency / audio->rate;
 	audio->samples = calloc(BUF_SIZE, sizeof(float));
 
-	SDL_Init(SDL_INIT_AUDIO);
-
-	SDL_AudioSpec spec = { 0 };
-	spec.freq = audio->rate;
-	spec.format = AUDIO_F32SYS;
-	spec.channels = channels;
-	spec.samples = BUF_SIZE;
-	audio->device = SDL_OpenAudioDevice(NULL, 0, &spec, NULL, 0);
-	SDL_PauseAudioDevice(audio->device, 0);
+	WAVEFORMATEX wfx = { 0 };
+	wfx.wFormatTag = WAVE_FORMAT_IEEE_FLOAT;
+	wfx.wBitsPerSample = 32;
+	wfx.nSamplesPerSec = rate;
+	wfx.nChannels = channels;
+	wfx.nBlockAlign = wfx.nChannels * wfx.wBitsPerSample / 8;
+	wfx.nAvgBytesPerSec = wfx.nSamplesPerSec * wfx.nBlockAlign;
+	waveOutOpen(&audio->hWaveOut, WAVE_MAPPER, &wfx, 0, 0, CALLBACK_FUNCTION);
 
 	return audio;
 }
@@ -49,13 +61,18 @@ int audio_frame_ready(struct audio *audio) {
 }
 
 void audio_frame_commit(struct audio *audio) {
-	SDL_QueueAudio(audio->device, audio->samples, audio->samples_count * sizeof(float));
+	LPWAVEHDR lpWaveHdr = calloc(1, sizeof(WAVEHDR));
+	lpWaveHdr->dwBufferLength = audio->samples_count * sizeof(float);
+	lpWaveHdr->lpData = calloc(1, lpWaveHdr->dwBufferLength);
+	memcpy(lpWaveHdr->lpData, audio->samples, lpWaveHdr->dwBufferLength);
+	waveOutPrepareHeader(audio->hWaveOut, lpWaveHdr, sizeof(WAVEHDR));
+	waveOutWrite(audio->hWaveOut, lpWaveHdr, sizeof(WAVEHDR));
 	audio->samples_count = 0;
 }
 
 void audio_destroy(struct audio *audio) {
-	SDL_CloseAudioDevice(audio->device);
-	SDL_Quit();
+	waveOutReset(audio->hWaveOut);
+	waveOutClose(audio->hWaveOut);
 	free(audio->samples);
 	free(audio);
 }
