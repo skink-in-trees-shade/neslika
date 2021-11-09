77236d64cd693db827de614a82689aa043199274
skink-in-trees-shade <skink-in-trees-shade@users.noreply.github.com>
2021-11-09 21:58:12 +0100

    CNROM's CHR is ROM

--- src/cartridge/mapper/cnrom/cnrom.c
+++ src/cartridge/mapper/cnrom/cnrom.c
@@ -3,7 +3,6 @@
 #include "memory/read_name_table.h"
 #include "memory/read_program_rom.h"
 #include "memory/select_chr_rom_bank.h"
-#include "memory/write_character_rom.h"
 #include "memory/write_name_table.h"
 #include "cnrom.h"
 
@@ -13,6 +12,6 @@ void cnrom_load(struct cartridge *cartridge) {
 	cartridge->mapper_data = cnrom;
 
 	bus_register(cartridge->cpu_bus, cartridge, 0x8000, 0xFFFF, &cnrom_read_program_rom, &cnrom_select_chr_rom_bank);
-	bus_register(cartridge->ppu_bus, cartridge, 0x0000, 0x1FFF, &cnrom_read_character_rom, &cnrom_write_character_rom);
+	bus_register(cartridge->ppu_bus, cartridge, 0x0000, 0x1FFF, &cnrom_read_character_rom, NULL);
 	bus_register(cartridge->ppu_bus, cartridge, 0x2000, 0x3EFF, &cnrom_read_name_table, &cnrom_write_name_table);
 }
