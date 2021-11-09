7942349ef6d8018281a85edb5c9ee37d770d9f76
skink-in-trees-shade <skink-in-trees-shade@users.noreply.github.com>
2021-11-09 21:58:32 +0100

    PxROM's CHR is ROM

--- src/cartridge/mapper/pxrom/memory/write_character_rom.h
+++
@@ -1,8 +0,0 @@
-#ifndef PXROM_WRITE_CHARACTER_ROM_H
-#define PXROM_WRITE_CHARACTER_ROM_H
-
-#include <stdint.h>
-
-void pxrom_write_character_rom(void *device, uint16_t address, uint8_t value);
-
-#endif
