9a35da63cd6c55d6abd159d20146622d914cec82
skink-in-trees-shade <skink-in-trees-shade@users.noreply.github.com>
2021-11-09 21:58:12 +0100

    CNROM's CHR is ROM

--- src/cartridge/mapper/cnrom/memory/write_character_rom.h
+++
@@ -1,8 +0,0 @@
-#ifndef CNROM_WRITE_CHARACTER_ROM_H
-#define CNROM_WRITE_CHARACTER_ROM_H
-
-#include <stdint.h>
-
-void cnrom_write_character_rom(void *device, uint16_t address, uint8_t value);
-
-#endif
